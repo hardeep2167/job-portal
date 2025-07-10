141f65c1ba1c10890a39cf8158e852f7a65b1890
hardeep2167 <srihari15007@gmail.com>
2025-07-10 13:13:44 +0530

    Update FileUploadService.java

--- src/main/java/com/example/ZidioProject/service/FileUploadService.java
+++ src/main/java/com/example/ZidioProject/service/FileUploadService.java
@@ -1,43 +1,4 @@
-//package com.example.ZidioProject.service;
-//
-//import com.cloudinary.Cloudinary;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.nio.file.StandardCopyOption;
-//import java.util.UUID;
-//
-//@Service
-//public class FileUploadService {
-//    @Value("${file.upload-dir}")
-//    private String uploadDir;
-//
-//    @Autowired
-//    private Cloudinary cloudinary;
-//
-//
-//    public String upload(MultipartFile file) throws IOException {
-//        Path path = Paths.get(uploadDir);
-//        if (!Files.exists(path)) {
-//            Files.createDirectories(path); // Fixed: exist -> exists
-//        }
-//
-//        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename(); // Fixed typo
-//        Path targetPath = path.resolve(fileName);
-//        Files.copy(file.getInputStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
-//
-//        return "/files/" + fileName;
-//    }
-//}
 
-
-//---------------------------------------
 
 package com.example.ZidioProject.service;
 
